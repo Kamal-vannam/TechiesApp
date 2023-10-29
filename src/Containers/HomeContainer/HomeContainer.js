a85ec37447b9b873891a98809a599dbfe5b46407
pavan <kumar@pavans-MacBook-Air.local>
2023-10-30 00:26:35 +0530

    Home Screen

--- src/Containers/HomeContainer/HomeContainer.js
+++ src/Containers/HomeContainer/HomeContainer.js
@@ -1,25 +1,76 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { black } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import SwiperFlatList from 'react-native-swiper-flatlist';
+import EvilIcons from "react-native-vector-icons/EvilIcons";
 
-const HomeContainer= () => {
 
+const HomeContainer = () => {
 
-    // const Catergories=[
-    //     {
-    //         id:1,
-    //         catergory_image:""
-    //         catergory_name:"",
-    //         catergory_specification:"",
-    //         catergory_weight:,
-    //         catergory_price:
-
-    //     },
-        
-    // ]
+    const data=[
+        {
+            "id":1,
+            "image":require('../../Images/SwiperImages/meat.jpg')
+        },
+        {
+            "id":2,
+            "image":require('../../Images/SwiperImages/meatadd2.jpg')
+        },
+        {
+            "id":3,
+            "image":require('../../Images/SwiperImages/keema.jpg')
+        },
+        {
+            "id":4,
+            "image":require('../../Images/SwiperImages/bbm.jpg')
+        }
+    ]
     return (
-        <View>
-            <Text>HomeContainer</Text>
-        </View>
+        <SafeAreaView>
+            <View style={{
+                flexDirection: 'row',
+                padding: 5,
+
+            }}>
+                <EvilIcons
+                    name="location"
+                    size={40}
+                    color={"black"}
+                />
+                <Text style={{
+                    marginTop: 5,
+                    fontWeight: "bold",
+                    color: 'black'
+                }}>Hyderabad</Text>
+
+                <EvilIcons
+                    name='chevron-down'
+                    size={35}
+                />
+
+            </View>
+            <SwiperFlatList
+          autoplay
+          autoplayDelay={3}
+          autoplayLoop
+          index={2}
+          showPagination
+          // PaginationComponent={CustomPagination}
+          data={data}
+          renderItem={({ item }) => (
+            <TouchableOpacity>
+              <Image
+                source={item.image}
+                resizeMode="cover"
+                style={{ height: 200, width: 400, }}
+              />
+            </TouchableOpacity>
+          )}
+        />
+
+
+        </SafeAreaView>
     )
 }
 

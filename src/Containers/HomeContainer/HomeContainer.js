ce5becbb7e6e1585c3d2026a3a18a0db003100a6
pavan <kumar@pavans-MacBook-Air.local>
2023-10-30 23:21:59 +0530

    Home screen desing

--- src/Containers/HomeContainer/HomeContainer.js
+++ src/Containers/HomeContainer/HomeContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity, FlatList } from 'react-native';
+import { View, Text, Image, TouchableOpacity, FlatList, ScrollView } from 'react-native';
 import { Card } from 'react-native-paper';
 import { black } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -32,98 +32,211 @@ const HomeContainer = () => {
         {
             id: 1,
             name: 'Chicken',
-            'image': require('../../Images/SwiperImages/chi.jpg')
+            'image': require('../../Images/SwiperImages/chi.jpg'),
+            grm: '500 g',
+            price: 150
         },
         {
             id: 2,
             name: 'Eggs',
-            'image': require('../../Images/SwiperImages/egg.jpg')
+            'image': require('../../Images/SwiperImages/egg.jpg'),
+            grm: "12 Eggs",
+            price: 100
         },
         {
             id: 3,
             name: 'Fish',
-            'image': require('../../Images/SwiperImages/fish.jpg')
+            'image': require('../../Images/SwiperImages/fish.jpg'),
+            grm: '500 g',
+            price: 150
         },
         {
             id: 4,
             name: 'Mutton',
-            'image': require('../../Images/SwiperImages/mut.jpg')
+            'image': require('../../Images/SwiperImages/mut.jpg'),
+            grm: '500 g',
+            price: 300
+        }
+    ]
+
+    const Categories = [
+        {
+            id: 1,
+            name: 'Chicken',
+            'image': require('../../Images/SwiperImages/chi.jpg'),
+
+        },
+        {
+            id: 2,
+            name: 'Eggs',
+            'image': require('../../Images/SwiperImages/egg.jpg'),
+
+        },
+        {
+            id: 3,
+            name: 'Fish',
+            'image': require('../../Images/SwiperImages/fish.jpg'),
+
+        },
+        {
+            id: 4,
+            name: 'Mutton',
+            'image': require('../../Images/SwiperImages/mut.jpg'),
+
+        },
+        {
+            id:5,
+            name:'Liver',
+            'image':require('../../Images/SwiperImages/liver.jpg')
+        },
+        {
+            id:6,
+            name:' skin',
+            'image':require('../../Images/SwiperImages/skin.jpg')
+        },
+        {
+            id:7,
+            name:'Mutton Boti',
+            'image':require('../../Images/SwiperImages/boti.jpg')
+        },
+        {
+            id:8,
+            name:'Boneless ',
+            'image':require('../../Images/SwiperImages/boneless.jpg')
+        },
+        {
+            id:9,
+            name:'Mutton',
+            'image':require('../../Images/SwiperImages/raan.jpg')
         }
     ]
     return (
-        <SafeAreaView>
-            <View style={{
-                flexDirection: 'row',
-                padding: 5,
+        <ScrollView>
+            <SafeAreaView>
+                {/* <ScrollView> */}
+                <View style={{
+                    flexDirection: 'row',
+                    padding: 5,
 
-            }}>
-                <EvilIcons
-                    name="location"
-                    size={40}
-                    color={"black"}
-                />
-                <Text style={{
-                    marginTop: 5,
-                    fontWeight: "bold",
-                    color: 'black'
-                }}>Hyderabad</Text>
+                }}>
+                    <EvilIcons
+                        name="location"
+                        size={40}
+                        color={"black"}
+                    />
+                    <Text style={{
+                        marginTop: 5,
+                        fontWeight: "bold",
+                        color: 'black'
+                    }}>Hyderabad</Text>
 
-                <EvilIcons
-                    name='chevron-down'
-                    size={35}
-                />
+                    <EvilIcons
+                        name='chevron-down'
+                        size={35}
+                    />
 
-            </View>
-            <View>
-                <SwiperFlatList
-                    autoplay
-                    autoplayDelay={3}
-                    autoplayLoop
-                    index={2}
-                    showPagination
-                    // PaginationComponent={CustomPagination}
-                    data={data}
-                    renderItem={({ item }) => (
-                        <TouchableOpacity>
-                            <Image
-                                source={item.image}
-                                resizeMode="cover"
-                                style={{ height: 200, width: 400, }}
-                            />
-                        </TouchableOpacity>
-                    )}
-                />
-            </View>
-            <View>
-                <FlatList
-                    data={menu}
-                    horizontal={true}
-                    renderItem={({ item }) => {
-                        return (
-                            <Card style={{
-                                marginLeft: 20,
-                                justifyContent: 'space-between',
-                                alignItems: 'center',
-                                marginTop: '30%'
-                            }}>
+                </View>
+                <View>
+                    <SwiperFlatList
+                        autoplay
+                        autoplayDelay={3}
+                        autoplayLoop
+                        index={2}
+                        showPagination
+                        // PaginationComponent={CustomPagination}
+                        data={data}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity>
                                 <Image
                                     source={item.image}
-                                    style={{ height: 100, width: 100, margin: 10 }}
+                                    resizeMode="cover"
+                                    style={{ height: 200, width: 400, }}
                                 />
-                                <Text style={{ alignSelf: 'center',
-                              fontWeight:'bold',
-                              color:'black'
-                             }}>{item.name}</Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </View>
+                <View style={{ padding:5}}>
+                    
+                    <FlatList
+                        data={menu}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={({ item }) => {
+                            return (
+                                <Card style={{
+                                    marginLeft: 20,
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',
+                                    marginTop: '30%',
+                                }}>
+                                    <Image
+                                        source={item.image}
+                                        style={{ height: 100, width: 100, margin: 10 }}
+                                    />
+                                    <Text style={{
+                                        alignSelf: 'center',
+                                        fontWeight: 'bold',
+                                        color: 'black'
+                                    }}>{item.name}</Text>
+                                    <Text style={{
+                                        alignSelf: 'center',
+                                        fontWeight: 'bold',
+                                        color: 'black'
+                                    }}>{item.grm}</Text>
+                                    <Text style={{
+                                        alignSelf: 'center',
+                                        fontWeight: 'bold',
+                                        color: 'black'
+                                    }}>Price:{item.price}</Text>
 
-                            </Card>
-                        )
-                    }}
-                />
-            </View>
+                                </Card>
+                            )
+                        }}
+                    />
+                </View>
+
+                <Card style={{
+                    width: '90%',
+                    alignSelf: 'center',
+                    marginTop: '10%',
+                    padding: 20
+                }}>
+                    <Text style={{
+                        color: 'black',
+                        fontWeight: 'bold',
+                        fontSize:20
+                    }}>Categories</Text>
+
+                    <FlatList
+                        data={Categories}
+                        // horizontal={true}
+                        numColumns={3}
+                        showsHorizontalScrollIndicator={false}
+                        renderItem={({ item }) => {
+                            return (
+                                <View style={{ padding:10}}>
+                                    <Image
+                                        source={item.image}
+                                        style={{ height: 80, width: 80, borderRadius: 80 / 2 }}
+                                    />
+                                    <Text style={{
+                                        color: 'black',
+                                        fontWeight: "bold",
+                                        alignSelf: 'center'
+                                    }}>{item.name}</Text>
+                                </View>
+                            )
+                        }}
+                    />
+
+                </Card>
+                {/* </ScrollView> */}
 
 
 
-        </SafeAreaView>
+            </SafeAreaView>
+        </ScrollView>
     )
 }
 

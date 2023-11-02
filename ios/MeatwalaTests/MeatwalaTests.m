3647abc4ffe99e6dfff90c5f70e2c7bed0ded297
Vannam kamal <vannamkamal1998@gmail.com>
2023-11-02 23:16:43 +0530

    new 0.69

--- ios/MeatwalaTests/MeatwalaTests.m
+++ ios/MeatwalaTests/MeatwalaTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TechiesAppTests : XCTestCase
+@interface MeatwalaTests : XCTestCase
 
 @end
 
-@implementation TechiesAppTests
+@implementation MeatwalaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
